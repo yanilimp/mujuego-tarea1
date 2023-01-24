65a0324367f68173a6ba72e2c9429d27e476aeb5
Yani Limprich <lizbethyani@gmail.com>
2023-01-24 17:53:25 -0300

    add title and buttons

--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -2,14 +2,30 @@
 /* eslint-disable import/namespace */
 /* eslint-disable prettier/prettier */
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 
 import { styles } from "./styles.js";
 
 export const StartGame = () => {
     return(
         <View style={styles.container}>
-            <Text> Start Game </Text>
+            <Text style={styles.title}> Start Game </Text>
+            <View style={styles.inputContainer}>
+                <Text>Escribe un número</Text>
+                <TextInput style={styles.input} placeholder="0" />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button
+                    title="Reiniciar"
+                    onPress={()=>null}
+                    color='#22577a'
+                />
+                <Button
+                    title="Confirmar"
+                    onPress={()=>null}
+                    color='#80ed99'
+                />
+            </View>
         </View>
     )
 }
